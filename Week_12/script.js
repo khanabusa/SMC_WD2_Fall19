997f8bcc2390e75c3aecef8f84bd9de9ccf25872
kelly <hanabusa_kelly_mieko@student.smc.edu>
2019-11-12 21:36:51 -0800

    Week 12 In class
    Jquerry effects with drop down

---
+++ Week_12/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    $('input, label').focus(function(){
+        console.log( $(this).attr('id') );
+        if( $(this).val() == ""){
+            $('main section.notification-area').append('<p class="notification">Please fill '+ $(this).attr('id') +' out</p>');
+        }
+    });
+
+    $('input, label').blur(function(){
+        $('p').remove()
+    });
+
+    $('select#FX').change(function(){
+        var currentSelection = $(this).val();
+        if(currentSelection === 'show'){
+            $('.box').show(1000);
+        }
+        if(currentSelection === 'hide') {
+            $('.box').hide(1000), function
+            $('main section.notification-area').append('<p class="notification">box hidden</p>');
+        }
+        if(currentSelection === 'slidedown') {
+            $('.box').slideDown(1000);
+        }
+        if(currentSelection === 'slideup')  {
+            $('.box').slideUp(1000);
+        }
+    })
+});
